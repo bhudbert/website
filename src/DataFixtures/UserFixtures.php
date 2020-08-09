02b33cdb0d70e5f35b88ae210c1769a651dd0ccd
Bruno HUDBERT <dev@bruno.hudbert.fr>
2020-08-09 16:44:54 +0200

    Adding Authentification Form and User Fixtures

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use App\Entity\User;
+
+class UserFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        // Password 1234 hash
+        $password='$argon2id$v=19$m=65536,t=4,p=1$d0hiV0NzOHlFMXdHVFVtZg$W3ZuXqaMaTmRMWHuRx/SCY3KGy2FjRzOUVCfh87/l0E';
+
+        $users[]=['bhudbert',['SA','A'],$password];
+        $users[]=['bruno',['U'],$password];
+
+        foreach ($users as $user) {
+            $newUser=new User();
+            $newUser->setUsername($user[0]);
+            $newUser->setRoles($user[1]);
+            $newUser->setPassword($user[2]);
+            $manager->persist($newUser);
+            $manager->flush();
+        }
+    }
+
+    public function getOrder()
+    {
+        return 1;
+    }
+}
